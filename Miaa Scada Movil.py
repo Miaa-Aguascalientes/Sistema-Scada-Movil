4e5544386a62de610ea8530c62422a8a69fe0a78
Miaa <pedroguillermomiaa@gmail.com>
2026-05-21 11:01:59 -0600

    Update Miaa Scada Movil.py

--- Miaa Scada Movil.py
+++ Miaa Scada Movil.py
@@ -622,7 +622,7 @@ if st.session_state.activo_tipo == "Pozo" and st.session_state.activo_id != "-- 
             )
         )
         st.plotly_chart(fig, use_container_width=True)
-        st.divider()
+        st.markdown("<hr style='border: 2px solid white;'>", unsafe_allow_html=True)
 
 
 # ------------------------------------------------------------------------------ seccion de tanques ------------------------------------------------------------------------
