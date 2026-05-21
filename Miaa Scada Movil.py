75b769d0cf020a9d980a913d1a7bc07ebb301af7
Miaa <pedroguillermomiaa@gmail.com>
2026-05-21 08:33:27 -0600

    Update Miaa Scada Movil.py

--- Miaa Scada Movil.py
+++ Miaa Scada Movil.py
@@ -40,7 +40,7 @@ def get_mysql_telemetria_engine():
         engine = create_engine(
             f"mysql+mysqlconnector://{c['user']}:{pwd}@{c['host']}/{c['database']}",
             pool_recycle=3600,
-            pool_pre_ping=True
+            pool_pre_ping=True,
             connect_args={'connect_timeout': 10}
         )
         return engine
