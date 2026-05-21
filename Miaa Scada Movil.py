01c1b915f7be08cb759a2dfd6912487cd27f1297
Miaa <pedroguillermomiaa@gmail.com>
2026-05-21 09:54:43 -0600

    Update Miaa Scada Movil.py

--- Miaa Scada Movil.py
+++ Miaa Scada Movil.py
@@ -478,7 +478,7 @@ if st.session_state.activo_tipo == "Pozo" and st.session_state.activo_id != "-- 
     val_nivel_tq = float(data_tq.get(info_p['nivel_tanque'], (0.0, ""))[0])
 
 # Aquí inicia el botón desplegable para los indicadores
-    with st.expander("⚙️ Indicadores", expanded=True):
+    with st.expander("⚙️ Indicadores", expanded=False):
         # Fila 1: 3 elementos principales
         f1 = st.columns(3)
         renderizar_tarjeta_kpi(f1[0], "Caudal Prom", f"{get_avg(info_p['caudal'], df):,.2f}", "Lps", "#00d4ff")
